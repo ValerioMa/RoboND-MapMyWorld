24f9a1b31ed73187df3d1fb0769df294e0073b6b
Valerio Magnago <valerio.magnago@hotmail.it>
2019-09-16 21:30:37 +0200

    feat: Implementation of multiple color ball chasing

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -1,15 +1,19 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
-
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
 #include <list>
+
+namespace enc = sensor_msgs::image_encodings;
 
 ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    ROS_INFO_STREAM("Moving the arm to the center");
+    ROS_INFO_STREAM("Moving the arm: (lin_x, ang_z) = (" << lin_x << ", " << ang_z <<");" );
 
     // Create service msg
     ball_chaser::DriveToTarget srv;
@@ -21,48 +25,54 @@ void drive_robot(float lin_x, float ang_z)
         ROS_ERROR("Failed to call service safe_move");
 }
 
-void process_image_callback(const sensor_msgs::Image img)
+void process_image_callback(const sensor_msgs::ImageConstPtr& msg)
 {
     const double max_speed = 0.5;
     const double max_angular = 2.5;
 
-    double lin_x = 0;
-    double ang_z = 0; //max_angular;
 
-    int white_pixel = 255;    
-    bool ball_finded = false;
+    // Convert to rgb      
+    cv_bridge::CvImageConstPtr cv_ptr; 
+    cv_ptr = cv_bridge::toCvShare(msg,  enc::BGR8);
+    cv::Mat src_gray;
+    cvtColor( cv_ptr->image, src_gray, CV_BGR2GRAY );
 
+    /// Reduce the noise so we avoid false circle detection
+    cv::GaussianBlur( src_gray, src_gray, cv::Size(9, 9), 2, 2 );
+
+    /// Apply the Hough Transform to find the circles
+    std::vector<cv::Vec3f> circles;
+    //cv::threshold(src_gray, src_gray, 100, 255,cv::THRESH_TOZERO); //Threshold the gray
+    cv::HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 70, 35, 0, 0 );
+
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      cv::circle( src_gray, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      cv::circle( src_gray, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );
+   }
+
+  /// Show your results
+  cv::namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
+  cv::imshow( "Hough Circle Transform Demo", src_gray );
+
+  cv::waitKey(10);
+
+  double lin_x = 0;
+  double ang_z = 0; //max_angular;
+  if(circles.size() != 1){
+    ROS_WARN_STREAM(circles.size() << " circles detected");  
+  }else{
     // Loop through each pixel in the image and check if its equal to the first one
-    //std::list<std::pair<int,int>> data_list;
-    double mean_i = 0, mean_j = 0;
-    size_t cnt = 0;    
-    for (int i = 0; i < img.height; i++) {
-      for(int j=0; (j+3) < img.step; j = j+3){
-        int idx = i*img.step + j;
-        if(img.data[idx] == white_pixel &&
-            img.data[idx + 1] == white_pixel &&
-            img.data[idx + 2] == white_pixel){
-            mean_i += 2*(i/(double)img.height - 0.5);
-            mean_j += 2*(j/(double)img.step - 0.5);
-            cnt++;
-            //data_list.emplace_back(i,j);
-        }
-      }        
-    }
-
-    if(cnt > 10){
-        ball_finded = true;
-        mean_i = mean_i/cnt;
-        mean_j = mean_j/cnt;
-        std::cout << "row position: " << mean_j << std::endl;
-
-        ang_z = - mean_j*max_angular;            
-        lin_x = (1 - std::abs(mean_j))*max_speed;
-        
-        
-    }
-
-    
+    ang_z = - (2*(circles[0][0]/(double)src_gray.cols) - 1.0)*max_angular;
+    double speed_scale = std::max(std::min(1., (90 - circles[0][2])/10.),0.);
+    lin_x = speed_scale*max_speed;
+  }
 
     // If the image is uniform and the arm is not moving, move the arm to the center
     drive_robot(lin_x, ang_z);    
